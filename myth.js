4baa6348513757394178e05b4346830a2d1c53d8
sdatta <sdatta@example.com>
2017-08-03 23:37:08 -0700

    module fix

--- myth.js
+++ myth.js
@@ -1,4 +1,213 @@
-define(["require", "exports", "./RNG", "./NameGen"], function (require, exports, RNG_1, NameGen_1) {
+define("RNG", ["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    var RNG = (function () {
+        function RNG(seed) {
+            this.seed = seed;
+        }
+        RNG.prototype.next = function (min, max) {
+            max = max || 0;
+            min = min || 0;
+            this.seed = (this.seed * 9301 + 49297) % 233280;
+            var rnd = this.seed / 233280;
+            return min + rnd * (max - min);
+        };
+        // http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+        RNG.prototype.nextInt = function (min, max) {
+            return Math.round(this.next(min, max));
+        };
+        RNG.prototype.nextDouble = function () {
+            return this.next(0, 1);
+        };
+        RNG.prototype.range = function (a, b) {
+            return this.next(a, b);
+        };
+        RNG.prototype.chance = function (ch) {
+            return this.nextDouble() < ch;
+        };
+        RNG.prototype.pick = function (collection) {
+            return collection[this.nextInt(0, collection.length - 1)];
+        };
+        RNG.prototype.pickW = function (collection, wts) {
+            if (collection.length != wts.length)
+                return undefined;
+            var s = 0;
+            for (var _i = 0, wts_1 = wts; _i < wts_1.length; _i++) {
+                var w = wts_1[_i];
+                s += w;
+            }
+            var ch = this.nextDouble() * s;
+            for (var i = 0; i < collection.length; i++) {
+                if (ch < wts[i])
+                    return collection[i];
+                ch -= wts[i];
+            }
+            return undefined;
+        };
+        RNG.prototype.shuffle = function (arr) {
+            for (var i = 0; i < arr.length; i++) {
+                var j = this.nextInt(0, arr.length - 1);
+                _a = [arr[j], arr[i]], arr[i] = _a[0], arr[j] = _a[1];
+            }
+            var _a;
+        };
+        return RNG;
+    }());
+    exports.RNG = RNG;
+});
+define("NameGen", ["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    /**
+     * @file A fantasy name generator library.
+     * @version 1.0.0
+     * @license Public Domain
+     *
+     * This library is designed after the RinkWorks Fantasy Name Generator.
+     * @see http://www.rinkworks.com/namegen/
+     * https://github.com/skeeto/fantasyname/blob/master/js/namegen.js
+     *
+     * @example
+     * var generator = NameGen.compile("sV'i");
+     * generator.toString();  // Emits a new name on each call
+     * // => "entheu'loaf"
+     *
+     * ## Pattern Syntax
+     *
+     *   The compile() function creates a name generator based on an input
+     * pattern. The letters s, v, V, c, B, C, i, m, M, D, and d represent
+     * different types of random replacements. Everything else is emitted
+     * literally.
+     *
+     *   s - generic syllable
+     *   v - vowel
+     *   V - vowel or vowel combination
+     *   c - consonant
+     *   B - consonant or consonant combination suitable for beginning a word
+     *   C - consonant or consonant combination suitable anywhere in a word
+     *   i - insult
+     *   m - mushy name
+     *   M - mushy name ending
+     *   D - consonant suited for a stupid person's name
+     *   d - syllable suited for a stupid person's name (begins with a vowel)
+     *
+     *   All characters between parenthesis () are emitted literally. For
+     * example, the pattern "s(dim)", emits a random generic syllable
+     * followed by "dim".
+     *
+     *   Characters between angle brackets <> emit patterns from the table
+     * above. Imagine the entire pattern is wrapped in one of these.
+     *
+     *   In both types of groupings, a vertical bar | denotes a random
+     * choice. Empty groups are allowed. For example, "(foo|bar)" emits
+     * either "foo" or "bar". The pattern "<c|v|>" emits a constant,
+     * vowel, or nothing at all.
+     *
+     *   An exclamation point ! means to capitalize the component that
+     * follows it. For example, "!(foo)" will emit "Foo" and "v!s" will
+     * emit a lowercase vowel followed by a capitalized syllable, like
+     * "eRod".
+     *
+     *   A tilde ~ means to reverse the letters of the component that
+     * follows it. For example, "~(foo)" will emit "oof". To reverse an
+     * entire template, wrap it in brackets. For example, to reverse
+     * "sV'i" as a whole use "~<sV'i>". The template "~sV'i" will only
+     * reverse the initial syllable.
+     *
+     * ## Internals
+     *
+     *   A name generator is anything with a toString() method, including,
+     * importantly, strings themselves. The generator constructors
+     * (Random, Sequence) perform additional optimizations when *not* used
+     * with the `new` keyword: they may pass through a provided generator,
+     * combine provided generators, or even return a simple string.
+     *
+     *   New pattern symbols added to NameGen.symbols will automatically
+     * be used by the compiler.
+     */
+    var symbols = {
+        s: ['ach', 'ack', 'ad', 'age', 'ald', 'ale', 'an', 'ang', 'ar', 'ard',
+            'as', 'ash', 'at', 'ath', 'augh', 'aw', 'ban', 'bel', 'bur', 'cer',
+            'cha', 'che', 'dan', 'dar', 'del', 'den', 'dra', 'dyn', 'ech', 'eld',
+            'elm', 'em', 'en', 'end', 'eng', 'enth', 'er', 'ess', 'est', 'et',
+            'gar', 'gha', 'hat', 'hin', 'hon', 'ia', 'ight', 'ild', 'im', 'ina',
+            'ine', 'ing', 'ir', 'is', 'iss', 'it', 'kal', 'kel', 'kim', 'kin',
+            'ler', 'lor', 'lye', 'mor', 'mos', 'nal', 'ny', 'nys', 'old', 'om',
+            'on', 'or', 'orm', 'os', 'ough', 'per', 'pol', 'qua', 'que', 'rad',
+            'rak', 'ran', 'ray', 'ril', 'ris', 'rod', 'roth', 'ryn', 'sam',
+            'say', 'ser', 'shy', 'skel', 'sul', 'tai', 'tan', 'tas', 'ther',
+            'tia', 'tin', 'ton', 'tor', 'tur', 'um', 'und', 'unt', 'urn', 'usk',
+            'ust', 'ver', 'ves', 'vor', 'war', 'wor', 'yer'],
+        v: ['a', 'e', 'i', 'o', 'u', 'y'],
+        V: ['a', 'e', 'i', 'o', 'u', 'y', 'ae', 'ai', 'au', 'ay', 'ea', 'ee',
+            'ei', 'eu', 'ey', 'ia', 'ie', 'oe', 'oi', 'oo', 'ou', 'ui'],
+        c: ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
+            's', 't', 'v', 'w', 'x', 'y', 'z'],
+        B: ['b', 'bl', 'br', 'c', 'ch', 'chr', 'cl', 'cr', 'd', 'dr', 'f', 'g',
+            'h', 'j', 'k', 'l', 'll', 'm', 'n', 'p', 'ph', 'qu', 'r', 'rh', 's',
+            'sch', 'sh', 'sl', 'sm', 'sn', 'st', 'str', 'sw', 't', 'th', 'thr',
+            'tr', 'v', 'w', 'wh', 'y', 'z', 'zh'],
+        C: ['b', 'c', 'ch', 'ck', 'd', 'f', 'g', 'gh', 'h', 'k', 'l', 'ld', 'll',
+            'lt', 'm', 'n', 'nd', 'nn', 'nt', 'p', 'ph', 'q', 'r', 'rd', 'rr',
+            'rt', 's', 'sh', 'ss', 'st', 't', 'th', 'v', 'w', 'y', 'z'],
+        i: ['air', 'ankle', 'ball', 'beef', 'bone', 'bum', 'bumble', 'bump',
+            'cheese', 'clod', 'clot', 'clown', 'corn', 'dip', 'dolt', 'doof',
+            'dork', 'dumb', 'face', 'finger', 'foot', 'fumble', 'goof',
+            'grumble', 'head', 'knock', 'knocker', 'knuckle', 'loaf', 'lump',
+            'lunk', 'meat', 'muck', 'munch', 'nit', 'numb', 'pin', 'puff',
+            'skull', 'snark', 'sneeze', 'thimble', 'twerp', 'twit', 'wad',
+            'wimp', 'wipe'],
+        m: ['baby', 'booble', 'bunker', 'cuddle', 'cuddly', 'cutie', 'doodle',
+            'foofie', 'gooble', 'honey', 'kissie', 'lover', 'lovey', 'moofie',
+            'mooglie', 'moopie', 'moopsie', 'nookum', 'poochie', 'poof',
+            'poofie', 'pookie', 'schmoopie', 'schnoogle', 'schnookie',
+            'schnookum', 'smooch', 'smoochie', 'smoosh', 'snoogle', 'snoogy',
+            'snookie', 'snookum', 'snuggy', 'sweetie', 'woogle', 'woogy',
+            'wookie', 'wookum', 'wuddle', 'wuddly', 'wuggy', 'wunny'],
+        M: ['boo', 'bunch', 'bunny', 'cake', 'cakes', 'cute', 'darling',
+            'dumpling', 'dumplings', 'face', 'foof', 'goo', 'head', 'kin',
+            'kins', 'lips', 'love', 'mush', 'pie', 'poo', 'pooh', 'pook', 'pums'],
+        D: ['b', 'bl', 'br', 'cl', 'd', 'f', 'fl', 'fr', 'g', 'gh', 'gl', 'gr',
+            'h', 'j', 'k', 'kl', 'm', 'n', 'p', 'th', 'w'],
+        d: ['elch', 'idiot', 'ob', 'og', 'ok', 'olph', 'olt', 'omph', 'ong',
+            'onk', 'oo', 'oob', 'oof', 'oog', 'ook', 'ooz', 'org', 'ork', 'orm',
+            'oron', 'ub', 'uck', 'ug', 'ulf', 'ult', 'um', 'umb', 'ump', 'umph',
+            'un', 'unb', 'ung', 'unk', 'unph', 'unt', 'uzz']
+    };
+    var NameGen = (function () {
+        function NameGen(template, rand) {
+            this.template = template;
+            this.rand = rand;
+        }
+        NameGen.prototype.generate = function () {
+            var s = "";
+            var tt = this.rand.pick(this.template);
+            for (var i = 0, len = tt.length; i < len; i++) {
+                if (symbols[tt[i]]) {
+                    s += this.rand.pick(symbols[tt[i]]);
+                }
+                else {
+                    s += tt[i];
+                }
+            }
+            //s = s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+            return s;
+        };
+        return NameGen;
+    }());
+    exports.NameGen = NameGen;
+});
+/*
+let rand:RNG = new RNG(Math.random() * 99999999);
+
+let n = new NameGen(["sV'dD", "ss'sV"], rand);
+
+for (var i = 0; i < 10; i++)
+{
+    console.log(n.generate());
+}
+*/ 
+define("myth", ["require", "exports", "RNG", "NameGen"], function (require, exports, RNG_1, NameGen_1) {
     "use strict";
     exports.__esModule = true;
     var C;
